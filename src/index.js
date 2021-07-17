264a0ec4ae2fa11570012d7634db14f89770f84f
Gaurav Sharma <gaurav@viewlift.com>
2021-07-17 12:18:51 +0530

    post css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style/index.scss'
+
 const obj = {
     a: 1,
     b: 2
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
-let mode = process.env.NODE_ENV === "production" ? "production" : "development"
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+let mode = "development"
+let target = 'web';
+
+if(process.env.NODE_ENV === "production"){
+    mode = 'production'
+    target = 'browserslist'
+}
+
 
 module.exports = {
     mode: mode,
-
+    target : target,
     module: {
         rules: [
             {
@@ -11,11 +20,20 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test : /\.s?css$/i,
+                use : [MiniCssExtractPlugin.loader, 'css-loader','postcss-loader', 'sass-loader']
             }
         ]
     },
+
+    plugins: [new MiniCssExtractPlugin()],
+
     devtool:'source-map',
+
     devServer: {
-        contentBase: "./dist"
+        contentBase: "./dist",
+        hot: true
     }
 }
